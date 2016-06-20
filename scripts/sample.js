808010f9440e1241de3ea797e59fbd6607fc4dd0
phoenix1796 <phoenix.prog1796@gmail.com>
2016-06-20 21:33:10 +0530

    Express test

--- scripts/sample.js
+++ scripts/sample.js
@@ -1,9 +1,10 @@
 module.exports = function(robot){
-
+	var express = require('express');
+	robot.express = app = express();
 	robot.hear(/hello/igm, function(msg){
 		msg.send("world!");
 	});
-
+	app.get('/',function(req,res){res.send("Hello")});
 	robot.respond(/show me the money/igm,function(msg){
 		msg.reply('filled :P');
 	});
